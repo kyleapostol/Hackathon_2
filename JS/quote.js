d56a54c845fcea187a515fa3abcff4a0e2958ead
raul jauregui <felx90@outlook.com>
2019-06-19 12:27:49 -0700

    quotev2test

--- JS/quote.js
+++ JS/quote.js
@@ -1,24 +1,42 @@
 class Quote {
 
     constructor(){
-
         $(".btn-success").on("click",this.randomQuote);
     }
-    randomQuote() {
+    randomQuote(weatherCondition) {
+
         var ajaxRandomQuote = {
-            url: "https://api.forismatic.com/api/1.0/?",
-            dataType: "jsonp",
-            data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+            url: "https://favqs.com/api/quotes/?filter="+weatherCondition,
+            headers: {
+                Authorization: 'Token token="8628443dc88e82625097a33570fd61cc"',
+
+            },
             success: function( response ) {
-                console.log(response);
                 $("#createQuote").html("<p id='createQuote' class='lead text-center'>" +
                     response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
 
                 $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
                     ' (' + response.quoteAuthor + ')');
             }
-         }
+         };
         $.ajax(ajaxRandomQuote);
     }
+
+    weatherQuote(weatherCondition){
+        for(var value in weatherCondition){
+
+        }
+    }r
+
+
+
+
+
+
+
+
+
+
+
 }
 
