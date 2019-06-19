b238ed374252f796e5db0599add970a891366251
Brady Greco <jbg1020@gmail.com>
2019-06-19 10:22:56 -0700

    removed dynamic created divs, added html divs, current location works

--- JS/darksky.js
+++ JS/darksky.js
@@ -1,25 +1,44 @@
 class DarkSkyObject{
     constructor(){
-        this.weatherInfo = {
-            url: 'https://api.darksky.net/forecast/5f2668d63b042969dc1581b9d2250cee/33.684566,-117.826508?exclude=alerts,flags?units=auto',
+        // debugger;
+        this.handleDataReceived = this.handleDataReceived.bind(this);
+        this.getLocation = this.getLocation.bind(this);
+        this.currentLocation = this.currentLocation.bind(this);
+        this.getLocation();
+    }
+    handleDataReceived(response) {
+        console.log("DarkSky response info!",response);
+        var temperature = "The current temperature is "+response.currently.temperature;
+        // var summary = response.currently.summary;
+        // var summary = response.minutely.summary;
+        var summary = response.hourly.summary;
+        // var summary = response.daily.summary;
+        $('#currentTemp').text(temperature);
+        $('#weatherSummary').text(summary);
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.currentLocation);
+          } else { 
+            alert( "Geolocation is not supported by this browser." );
+          }   
+    }
+
+    currentLocation(position) {
+        // debugger;
+        console.log('this: ', this);
+        console.log('position:', position.coords.latitude);
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var weatherInfo = {
+            // url: 'https://api.darksky.net/forecast/5f2668d63b042969dc1581b9d2250cee/33.684566,-117.826508?exclude=alerts,flags?units=auto', //will need to get current location 
+            url: 'https://api.darksky.net/forecast/5f2668d63b042969dc1581b9d2250cee/'+latitude+','+longitude+'?exclude=alerts,flags?units=auto',
             method: 'get',
             dataType: 'jsonp',
-            success: this.handleDataReceived, 
+            success: this.handleDataReceived,
         }
-        $.ajax(this.weatherInfo);
-        this.handleDataReceived = this.handleDataReceived.bind(this);
-    }
-    handleDataReceived(response) { //server response to the client
-        console.log(response);
-        var temperature = $('<div>').text("It is currently " +response.currently.temperature+ "°F");
-        var currentSummary = $('<div>').text("Current summary: " +response.currently.summary);
-        var minuteSummary = $('<div>').text("Minutely summary: " +response.minutely.summary);
-        var hourSummary = $('<div>').text("Hourly summary: " +response.hourly.summary);
-        var daySummary = $('<div>').text("Daily summary: " +response.daily.summary);
-        $('body').append(temperature);
-        $('body').append(currentSummary);
-        $('body').append(minuteSummary);
-        $('body').append(hourSummary);
-        $('body').append(daySummary);
-    }
+        $.ajax(weatherInfo);
+        
+      }
 }
