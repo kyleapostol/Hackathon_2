ad13ccb81bab42a426378976da3a9ece4a54f42b
Tony Jin <tonyjin23@gmail.com>
2019-06-19 12:34:43 -0700

    youtube file

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,13 @@
 $(document).ready(initApp);
 
+
 function initApp() {
+    var findYtData = new Youtube();
+    findYtData.ytData('guitar');
     var newDarkSky = new DarkSkyObject;
     newDarkSky.handleDataReceieved;
     var musicPlaylist = new MusixMatch;
     musicPlaylist.initClient; // pass in result from weather data
-    var random = new Quote();
+    var random = new Quote;
     random.randomQuote();
 }
